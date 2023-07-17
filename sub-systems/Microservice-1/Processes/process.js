ba61665d8edd76f0ff373d36eeff6321eca79c6c
anirudh-nayak-172k <anirudh.nayak.172k@gmail.com>
2023-07-17 22:58:22 +0530

    feature/register-user: process level design

--- sub-systems/Microservice-1/Processes/process.js
+++ sub-systems/Microservice-1/Processes/process.js
@@ -1,10 +1,51 @@
-const logger = require ('../../../shared/src/configurations/logger.configurations')
-
+const logger = require('../../../shared/src/configurations/logger.configurations');
+const bcrypt = require('bcryptjs');
+require('dotenv').config();
+const redisClient = require('../../../shared/src/configurations/redis.configurations.js');
 
 module.exports.Processes = {
-    coreProcess1: async (asd)=>{
-        logger.info(`This is the function argument : ${asd}`);
+  createNewUser: async ({ userName, email, password, phoneNo }) => {
+    try {
+      // Hash the password using bcrypt
+      const hashedPassword = bcrypt.hashSync(
+        password,
+        Number(process.env.HASHING_ROUNDS)
+      );
 
-        return asd
-    },
-}
+      // Prepare user details to be stored in Redis
+      const userDetails = [
+        { fieldName: 'userName', value: userName },
+        { fieldName: 'email', value: email },
+        { fieldName: 'password', value: hashedPassword },
+        { fieldName: 'phoneNo', value: phoneNo },
+      ];
+
+      // Check if the username already exists
+      const usernameExists = await redisClient.exists(`user:${userName}`);
+      if (usernameExists) {
+        throw {
+          status: 409,
+          message: 'Conflict',
+          error: 'Username already exists',
+        };
+      }
+
+      // Store user details in Redis
+      for (const userData of userDetails) {
+        await redisClient.hSet(
+          `user:${userName}`,
+          userData.fieldName,
+          userData.value
+        );
+      }
+
+      // Set username and password into a separate field for forgot password authentication
+      await redisClient.hSet(`user:${phoneNo}`, 'userName', userName);
+      await redisClient.hSet(`user:${phoneNo}`, 'password', hashedPassword);
+
+      return { message: 'User registered successfully' };
+    } catch (error) {
+      throw error;
+    }
+  },
+};
